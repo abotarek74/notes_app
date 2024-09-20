60fd471acf16dcd42952a7a85056242a79b8200f
abotarek74 <moustafatarek013@gmail.com>
2024-09-21 02:45:49 +0300

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddNoteCubit extends StatelessWidget {
+  const AddNoteCubit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
