fe5e2999b726b306147ed40b36f17090376853b0
abotarek74 <moustafatarek013@gmail.com>
2024-09-22 01:16:53 +0300

    Fix issue

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,6 +12,7 @@ class NotesView extends StatelessWidget {
         backgroundColor: const Color(0xff52ecd7),
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
